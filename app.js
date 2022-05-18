096f3b46b92025bb9f4df2964f3b82286df872b0
Zhen Ju <juzhen@huawei.com>
2022-05-18 18:13:30 +0800

    Support jsonable result. Handle clickhouse client error

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ const ckClient = new ck.CKClient(CK_HOST, CK_PORT, CK_USER, CK_PASS);
 const allowedOrigins = [];
 const ALLOWED_ORIGINS = process.env["ALLOWED_ORIGINS"];
 if (ALLOWED_ORIGINS) {
-  allowedOrigins.concat(ALLOWED_ORIGINS.split(","));
-  console.log("ALLOWED ORIGINS:", allowedOrigins);
-  // TODO Configure the origins
+    allowedOrigins.concat(ALLOWED_ORIGINS.split(","));
+    console.log("ALLOWED ORIGINS:", allowedOrigins);
+    // TODO Configure the origins
 }
 
 app.use(bodyParser.text());
@@ -28,43 +28,61 @@ app.use(cors());
 app.use(morgan("combined"));
 
 app.post("/sql/transfer", (req, res) => {
-  if (!!req.body == false || typeof req.body != "string") {
-    res.status(400);
-    res.send({
-      message: "Invalid SQL",
-    });
-    return;
-  }
-  ckClient.execute(req.body, (rows, cols) => {
-    // TODO Let clickhouse engine parse the data with correct type
-    // According to ClickHouse input / output format doc:
-    // https://clickhouse.com/docs/en/interfaces/formats/#jsoncompact
-    // Some output format should automatically parse value with type
-    // But even the propery output format is set, the Int value is still a string
-    // The python client automatically parses the data, check if it's done in the py code or by db
-    const columns = cols.map((col, colIndex) => {
-      if (ck.isIntType(col.type)) {
-        rows.forEach((row) => {
-          row[colIndex] = parseInt(row[colIndex]);
+    if (!!req.body == false || typeof req.body != "string") {
+        res.status(400);
+        res.send({
+            message: "Invalid SQL",
         });
-      } else if (ck.isFloatType(col.type)) {
-        rows.forEach((row) => {
-          row[colIndex] = parseFloat(row[colIndex]);
+        return;
+    }
+
+    const sqlOptions = {format: 'JSONCompact'}
+    if (req.header('jsonObjects') === 'true') {
+        sqlOptions.format = 'JSON'
+    }
+
+    ckClient.execute_with_options(req.body, sqlOptions, (rows, cols, err) => {
+        if (err) {
+            console.error(err)
+            res.status(500)
+            return res.send('Internal Server Error')
+        }
+
+        if (sqlOptions.format === 'JSON') {
+            return res.send([rows, cols])
+        }
+
+        // TODO Let clickhouse engine parse the data with correct type
+        // According to ClickHouse input / output format doc:
+        // https://clickhouse.com/docs/en/interfaces/formats/#jsoncompact
+        // Some output format should automatically parse value with type
+        // But even the propery output format is set, the Int value is still a string
+        // The python client automatically parses the data, check if it's done in the py code or by db
+        const columns = cols.map((col, colIndex) => {
+            if (ck.isIntType(col.type)) {
+                rows.forEach((row) => {
+                    row[colIndex] = parseInt(row[colIndex]);
+                });
+            } else if (ck.isFloatType(col.type)) {
+                rows.forEach((row) => {
+                    row[colIndex] = parseFloat(row[colIndex]);
+                });
+            }
+            return [col.name, col.type];
         });
-      }
-      return [col.name, col.type];
+        res.send([rows, columns]);
     });
-    res.send([rows, columns]);
-  });
+
+
 });
 app.post("/api/login/account", (req, res) => {
-  if (req.body.username == "admin" && req.body.password == "admin") {
-    res.status(200);
+    if (req.body.username == "admin" && req.body.password == "admin") {
+        res.status(200);
+        return res.send();
+    }
+    res.status(400);
     return res.send();
-  }
-  res.status(400);
-  return res.send();
 });
 app.listen(LISTEN_PORT, () => {
-  console.log(`Example app listening on port ${LISTEN_PORT}`);
+    console.log(`Dashboard backend app listening on port ${LISTEN_PORT}`);
 });
--- ck.js
+++ ck.js
@@ -1,74 +1,65 @@
 const ClickHouse = require("@apla/clickhouse");
 
 class CKClient {
-  constructor(host, port, user, password) {
-    this.ch = new ClickHouse({
-      host,
-      port,
-      user,
-      password,
-    });
-    this.connectionOpts = {
-      host,
-      port,
-      user,
-      password,
-    };
-  }
+    constructor(host, port, user, password) {
+        this.ch = new ClickHouse({
+            host,
+            port,
+            user,
+            password,
+        });
+    }
 
-  execute(sql, callback) {
-    // console.log(this.connectionOpts);
-    // // const ch = new ClickHouse(...this.connectionOpts);
-    // const ch = new ClickHouse({
-    //   host: "localhost",
-    //   port: 18000,
-    //   user: "querydata",
-    //   password: "querydata",
-    // });
-    const stream = this.ch.query(sql);
+    execute_with_options(sql, options, callback) {
+        const stream = this.ch.query(sql, options);
 
-    let columns = [];
-    stream.on("metadata", (cols) => {
-      columns = cols;
-    });
+        let columns = [];
+        stream.on("metadata", (cols) => {
+            columns = cols;
+        });
 
-    const rows = [];
-    stream.on("data", (row) => {
-      rows.push(row);
-    });
+        const rows = [];
+        stream.on("data", (row) => {
+            rows.push(row);
+        });
 
-    stream.on("end", () => {
-      callback(rows, columns);
-    });
-  }
+        stream.on("end", () => {
+            callback(rows, columns);
+        });
+
+        stream.on("error", (err) => {
+            console.log('stream err:', err)
+            callback(null, null, err)
+        })
+    }
 }
 
 const INT_TYPE_MAP = {
-  UInt8: true,
-  UInt16: true,
-  UInt32: true,
-  UInt64: true,
-  UInt128: true,
-  UInt256: true,
-  Int8: true,
-  Int16: true,
-  Int32: true,
-  Int64: true,
-  Int128: true,
-  Int256: true,
+    UInt8: true,
+    UInt16: true,
+    UInt32: true,
+    UInt64: true,
+    UInt128: true,
+    UInt256: true,
+    Int8: true,
+    Int16: true,
+    Int32: true,
+    Int64: true,
+    Int128: true,
+    Int256: true,
 };
 
 const FLOAT_TYPE_MAP = {
-  Float32: true,
-  Float64: true,
+    Float32: true,
+    Float64: true,
 };
 
 function isIntType(typeName) {
-  return INT_TYPE_MAP.hasOwnProperty(typeName);
+    return INT_TYPE_MAP.hasOwnProperty(typeName);
 }
 
 function isFloatType(typeName) {
-  return FLOAT_TYPE_MAP.hasOwnProperty(typeName);
+    return FLOAT_TYPE_MAP.hasOwnProperty(typeName);
 }
 
 module.exports.CKClient = CKClient;
