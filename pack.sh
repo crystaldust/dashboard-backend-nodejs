65b42d31e67e791d2ee3f8797eb0b9305d74022b
Zhen Ju <juzhenatpku@gmail.com>
2022-11-23 15:31:03 +0800

    Specify tag and org for docker image while buliding.

--- pack.sh
+++ pack.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+TAG=$1
+if [ -z "$1" ]; then
+	TAG=dev
+fi
+
+ORG=$2
+if [ -z "$1" ]; then
+	ORG=oss-know
+fi
+
 git log --format="%H" -n 1 > LAST_GIT_COMMIT
-docker build -t dashboard-backend-nodejs:dev ./
-docker save dashboard-backend-nodejs:dev | gzip > dashboard-backend-nodejs.dev.tgz
+docker build -t $ORG/dashboard-backend-nodejs:$TAG ./
+rm LAST_GIT_COMMIT
+
+# docker save dashboard-backend-nodejs:$TAG | gzip > dashboard-backend-nodejs.dev.tgz
